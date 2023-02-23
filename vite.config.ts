2f4f478649401c4029a595c2c911e7cc86dedf27
Shin You <shin_you@tomofun.com>
2023-02-23 14:22:53 +0800

    feat(vite): optimize performance

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 import react from '@vitejs/plugin-react';
+import viteCompression from 'vite-plugin-compression';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react({ include: '**/*.tsx' }),
+    viteCompression(),
     basicSsl(),
     {
       name: 'singleHMR',
